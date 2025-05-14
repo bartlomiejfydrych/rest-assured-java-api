5758ffbf92780fc249393e07c590c2f7986c46cd
bartlomiejfydrych <fydbar@gmail.com>
2025-05-14 22:23:25 +0200

    14.05.2025 - Dodanie walidacji do deserializacji

--- src/test/java/utils/responseUtils.java
+++ src/test/java/utils/responseUtils.java
@@ -1,10 +1,14 @@
 package utils;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class responseUtils {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
+            .configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, true)
+            .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true);
 
     public static <T> T deserializeJson(String json, Class<T> clazz) {
         try {
