4673a2c30d8a232c6ca5124891e347edadc21499
bartlomiejfydrych <fydbar@gmail.com>
2025-12-26 19:11:48 +0100

    26.12.2025 (test/configuration): Refaktoruj plik {RequestSpecConfig.java}

--- src/test/java/configuration/RequestSpecConfig.java
+++ src/test/java/configuration/RequestSpecConfig.java
@@ -6,12 +6,20 @@ import io.restassured.specification.RequestSpecification;
 
 public class RequestSpecConfig {
 
+    // ==========================================================================================================
+    // FIELDS
+    // ==========================================================================================================
+
     private static final RequestSpecification requestSpecification = new RequestSpecBuilder()
             .addQueryParam("key", Config.getTrelloApiKey())
             .addQueryParam("token", Config.getTrelloToken())
             .setBaseUri(BaseUrlBuilder.buildBaseUrl())
             .setContentType(ContentType.JSON)
             .build();
+
+    // ==========================================================================================================
+    // METHODS – MAIN
+    // ==========================================================================================================
 
     public static RequestSpecification getRequestSpecification() {
         return requestSpecification;
