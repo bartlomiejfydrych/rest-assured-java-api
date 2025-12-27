7812d47692cbd2dcbfe185a53ad545079823f8a7
bartlomiejfydrych <fydbar@gmail.com>
2025-12-27 20:17:17 +0100

    27.12.2025 (dto): Refaktoruj plik {GET_GetBoardDto.java}

--- src/test/java/dto/boards/GET_GetBoardDto.java
+++ src/test/java/dto/boards/GET_GetBoardDto.java
@@ -12,24 +12,29 @@ import java.net.URL;
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class GET_GetBoardDto extends BoardBaseDto {
 
+    // ==========================================================================================================
+    // CONSTRUCTORS
+    // ==========================================================================================================
+
     @JsonCreator
     public GET_GetBoardDto(
-            @JsonProperty(value = "id", required = true) String id,
-            @JsonProperty(value = "name", required = true) String name,
-            @JsonProperty(value = "desc", required = true) String desc,
-            @JsonProperty(value = "descData", required = true) DescData descData,
-            @JsonProperty(value = "closed", required = true) Boolean closed,
-            @JsonProperty(value = "idOrganization", required = true) String idOrganization,
-            @JsonProperty(value = "idEnterprise", required = true) Object idEnterprise,
-            @JsonProperty(value = "pinned", required = true) Boolean pinned,
-            @JsonProperty(value = "url", required = true) URL url,
-            @JsonProperty(value = "shortUrl", required = true) URL shortUrl,
-            @JsonProperty(value = "prefs", required = true) Prefs prefs,
-            @JsonProperty(value = "labelNames", required = true) LabelNames labelNames
+            @JsonProperty(value = FIELD_ID, required = true) String id,
+            @JsonProperty(value = FIELD_NAME, required = true) String name,
+            @JsonProperty(value = FIELD_DESC, required = true) String desc,
+            @JsonProperty(value = FIELD_DESC_DATA, required = true) DescData descData,
+            @JsonProperty(value = FIELD_CLOSED, required = true) Boolean closed,
+            @JsonProperty(value = FIELD_ID_ORGANIZATION, required = true) String idOrganization,
+            @JsonProperty(value = FIELD_ID_ENTERPRISE, required = true) Object idEnterprise,
+            @JsonProperty(value = FIELD_PINNED, required = true) Boolean pinned,
+            @JsonProperty(value = FIELD_URL, required = true) URL url,
+            @JsonProperty(value = FIELD_SHORT_URL, required = true) URL shortUrl,
+            @JsonProperty(value = FIELD_PREFS, required = true) Prefs prefs,
+            @JsonProperty(value = FIELD_LABEL_NAMES, required = true) LabelNames labelNames
     ) {
         super(id, name, desc, descData, closed, idOrganization, idEnterprise, pinned, url, shortUrl, prefs, labelNames);
     }
 
+    // Empty constructor - needed to be able to assign values manually
     public GET_GetBoardDto() {
         super();
     }
