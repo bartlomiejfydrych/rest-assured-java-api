a2b45895229d8487a698abe0c20b97be21a379cc
bartlomiejfydrych <fydbar@gmail.com>
2025-12-27 20:17:01 +0100

    27.12.2025 (dto): Refaktoruj plik {DescData.java}

--- src/test/java/dto/boards/board/DescData.java
+++ src/test/java/dto/boards/board/DescData.java
@@ -9,14 +9,29 @@ import jakarta.validation.constraints.NotNull;
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class DescData {
 
+    // ==========================================================================================================
+    // FIELDS
+    // ==========================================================================================================
+
+    public static final String FIELD_EMOJI = "emoji";
+
+    // ==========================================================================================================
+    // FIELDS – VALIDATION CONSTRAINTS
+    // ==========================================================================================================
+
     @NotNull
     public Emoji emoji;
 
+    // ==========================================================================================================
+    // CONSTRUCTORS
+    // ==========================================================================================================
+
     @JsonCreator
-    public DescData(@JsonProperty(value = "emoji", required = true) Emoji emoji) {
+    public DescData(@JsonProperty(value = FIELD_EMOJI, required = true) Emoji emoji) {
         this.emoji = emoji;
     }
 
+    // Empty constructor - needed to be able to assign values manually
     public DescData() {
     }
 }
