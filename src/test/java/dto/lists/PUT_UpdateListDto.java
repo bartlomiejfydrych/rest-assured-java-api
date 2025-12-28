142fc97b28088699e1ea91c6bf07364ceeba6c7c
bartlomiejfydrych <fydbar@gmail.com>
2025-12-28 18:57:01 +0100

    28.12.2025 (dto): Refaktoruj plik {PUT_UpdateListDto.java}

--- src/test/java/dto/lists/PUT_UpdateListDto.java
+++ src/test/java/dto/lists/PUT_UpdateListDto.java
@@ -3,29 +3,43 @@ package dto.lists;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.Valid;
 
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class PUT_UpdateListDto extends ListBaseDto {
 
+    // ==========================================================================================================
+    // FIELDS
+    // ==========================================================================================================
+
+    // NOTE:
+    // These variables are also used to call the name of an ignored, redundant field when comparing objects using AssertJ.
+
     public static final String FIELD_SUBSCRIBED = "subscribed";
 
-    @Valid
+    // ==========================================================================================================
+    // FIELDS – VALIDATION CONSTRAINTS
+    // ==========================================================================================================
+
     @JsonProperty(FIELD_SUBSCRIBED)
     public Boolean subscribed;
 
+    // ==========================================================================================================
+    // CONSTRUCTORS
+    // ==========================================================================================================
+
     @JsonCreator
     public PUT_UpdateListDto(
-            @JsonProperty(value = "id", required = true) String id,
-            @JsonProperty(value = "name", required = true) String name,
-            @JsonProperty(value = "closed", required = true) Boolean closed,
-            @JsonProperty(value = "color", required = true) String color,
-            @JsonProperty(value = "idBoard", required = true) String idBoard,
-            @JsonProperty(value = "pos", required = true) Long pos
+            @JsonProperty(value = FIELD_ID, required = true) String id,
+            @JsonProperty(value = FIELD_NAME, required = true) String name,
+            @JsonProperty(value = FIELD_CLOSED, required = true) Boolean closed,
+            @JsonProperty(value = FIELD_COLOR, required = true) String color,
+            @JsonProperty(value = FIELD_ID_BOARD, required = true) String idBoard,
+            @JsonProperty(value = FIELD_POS, required = true) Long pos
     ) {
         super(id, name, closed, color, idBoard, pos);
     }
 
+    // Empty constructor - needed to be able to assign values manually
     public PUT_UpdateListDto() {
         super();
     }
