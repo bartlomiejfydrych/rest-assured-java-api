49ce961e11ae6b19d039adcaca4cb8bd0702ea34
bartlomiejfydrych <fydbar@gmail.com>
2025-12-28 18:55:46 +0100

    28.12.2025 (dto): Refaktoruj plik {POST_CreateNewListDto.java}

--- src/test/java/dto/lists/POST_CreateNewListDto.java
+++ src/test/java/dto/lists/POST_CreateNewListDto.java
@@ -10,29 +10,44 @@ import jakarta.validation.constraints.NotNull;
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class POST_CreateNewListDto extends ListBaseDto {
 
-    // Note:
-    // This variable is used to call it as the name of an ignored, redundant field when comparing objects using AssertJ.
+    // ==========================================================================================================
+    // FIELDS
+    // ==========================================================================================================
+
+    // NOTE:
+    // These variables are also used to call the name of an ignored, redundant field when comparing objects using AssertJ.
+
+    public static final String FIELD_TYPE = "type";
+    public static final String FIELD_DATASOURCE = "datasource";
     public static final String FIELD_LIMITS = "limits";
+
+    // ==========================================================================================================
+    // FIELDS – VALIDATION CONSTRAINTS
+    // ==========================================================================================================
 
     public String type;
 
-    @Valid
+    @Valid // <-- validates nested fields if object exists
     @NotNull
     public DataSource datasource;
 
-    @Valid
+    @Valid // <-- validates nested fields if object exists
     public Object limits;
+
+    // ==========================================================================================================
+    // CONSTRUCTORS
+    // ==========================================================================================================
 
     @JsonCreator
     public POST_CreateNewListDto(
-            @JsonProperty(value = "id", required = true) String id,
-            @JsonProperty(value = "name", required = true) String name,
-            @JsonProperty(value = "closed", required = true) Boolean closed,
-            @JsonProperty(value = "color", required = true) String color,
-            @JsonProperty(value = "idBoard", required = true) String idBoard,
-            @JsonProperty(value = "pos", required = true) Long pos,
-            @JsonProperty(value = "type", required = true) String type,
-            @JsonProperty(value = "datasource", required = true) DataSource datasource,
+            @JsonProperty(value = FIELD_ID, required = true) String id,
+            @JsonProperty(value = FIELD_NAME, required = true) String name,
+            @JsonProperty(value = FIELD_CLOSED, required = true) Boolean closed,
+            @JsonProperty(value = FIELD_COLOR, required = true) String color,
+            @JsonProperty(value = FIELD_ID_BOARD, required = true) String idBoard,
+            @JsonProperty(value = FIELD_POS, required = true) Long pos,
+            @JsonProperty(value = FIELD_TYPE, required = true) String type,
+            @JsonProperty(value = FIELD_DATASOURCE, required = true) DataSource datasource,
             @JsonProperty(value = FIELD_LIMITS, required = true) Object limits
     ) {
         super(id, name, closed, color, idBoard, pos);
@@ -41,6 +56,7 @@ public class POST_CreateNewListDto extends ListBaseDto {
         this.limits = limits;
     }
 
+    // Empty constructor - needed to be able to assign values manually
     public POST_CreateNewListDto() {
         super();
     }
