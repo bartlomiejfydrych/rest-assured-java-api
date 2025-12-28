8e77dc4287f7357712f4d318cf59bee4b6cda5c3
bartlomiejfydrych <fydbar@gmail.com>
2025-12-28 18:54:10 +0100

    28.12.2025 (dto): Refaktoruj plik {GET_ListAvaiableEmojiDto.java}

--- src/test/java/dto/emoji/GET_ListAvailableEmojiDto.java
+++ src/test/java/dto/emoji/GET_ListAvailableEmojiDto.java
@@ -13,21 +13,41 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class GET_ListAvailableEmojiDto {
 
-    @Valid
-    @JsonProperty("spriteSheets")
+    // ==========================================================================================================
+    // FIELDS
+    // ==========================================================================================================
+
+    // NOTE:
+    // These variables are also used to call the name of an ignored, redundant field when comparing objects using AssertJ.
+
+    public static final String FIELD_SPRITE_SHEETS = "spriteSheets";
+
+    public static final String FIELD_TRELLO = "trello";
+
+    // ==========================================================================================================
+    // FIELDS – VALIDATION CONSTRAINTS
+    // ==========================================================================================================
+
+    @Valid // <-- validates nested fields if object exists
+    @JsonProperty(FIELD_SPRITE_SHEETS)
     public SpriteSheets spriteSheets;
 
     @NotNull
-    @Valid
+    @Valid // <-- validates nested fields if object exists
     public List<Trello> trello;
+
+    // ==========================================================================================================
+    // CONSTRUCTORS
+    // ==========================================================================================================
 
     @JsonCreator
     public GET_ListAvailableEmojiDto(
-            @JsonProperty(value = "trello", required = true) List<Trello> trello
+            @JsonProperty(value = FIELD_TRELLO, required = true) List<Trello> trello
     ) {
         this.trello = trello;
     }
 
+    // Empty constructor - needed to be able to assign values manually
     public GET_ListAvailableEmojiDto() {
     }
 }
