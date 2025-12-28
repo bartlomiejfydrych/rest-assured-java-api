4fda32a62ced0ca1a2d9751e056569ea3104e09a
bartlomiejfydrych <fydbar@gmail.com>
2025-12-28 18:58:19 +0100

    28.12.2025 (dto): Refaktoruj plik {Twitter.java}

--- src/test/java/dto/emoji/list_available_emoji/sprite_sheets/Twitter.java
+++ src/test/java/dto/emoji/list_available_emoji/sprite_sheets/Twitter.java
@@ -9,26 +9,47 @@ import jakarta.validation.constraints.NotNull;
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class Twitter {
 
+    // ==========================================================================================================
+    // FIELDS
+    // ==========================================================================================================
+
+    // NOTE:
+    // These variables are also used to call the name of an ignored, redundant field when comparing objects using AssertJ.
+
+    public static final String FIELD_16 = "16";
+    public static final String FIELD_20 = "20";
+    public static final String FIELD_32 = "32";
+    public static final String FIELD_64 = "64";
+
+    // ==========================================================================================================
+    // FIELDS – VALIDATION CONSTRAINTS
+    // ==========================================================================================================
+
     @NotNull
-    @Valid
-    @JsonProperty("16")
+    @Valid // <-- validates nested fields if object exists
+    @JsonProperty(FIELD_16)
     public SpriteSheetEntry size16;
 
     @NotNull
-    @Valid
-    @JsonProperty("20")
+    @Valid // <-- validates nested fields if object exists
+    @JsonProperty(FIELD_20)
     public SpriteSheetEntry size20;
 
     @NotNull
-    @Valid
-    @JsonProperty("32")
+    @Valid // <-- validates nested fields if object exists
+    @JsonProperty(FIELD_32)
     public SpriteSheetEntry size32;
 
     @NotNull
-    @Valid
-    @JsonProperty("64")
+    @Valid // <-- validates nested fields if object exists
+    @JsonProperty(FIELD_64)
     public SpriteSheetEntry size64;
 
+    // ==========================================================================================================
+    // CONSTRUCTORS
+    // ==========================================================================================================
+
+    // Empty constructor - needed to be able to assign values manually
     public Twitter() {
     }
 }
