750b0152ed2a2a52af806d30db76beb8dfc34431
bartlomiejfydrych <fydbar@gmail.com>
2025-12-28 18:53:13 +0100

    28.12.2025 (dto): Refaktoruj plik {DataSource.java}

--- src/test/java/dto/lists/list/DataSource.java
+++ src/test/java/dto/lists/list/DataSource.java
@@ -8,16 +8,34 @@ import jakarta.validation.constraints.NotNull;
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class DataSource {
 
+    // ==========================================================================================================
+    // FIELDS
+    // ==========================================================================================================
+
+    // NOTE:
+    // These variables are also used to call the name of an ignored, redundant field when comparing objects using AssertJ.
+
+    public static final String FIELD_FILTER = "filter";
+
+    // ==========================================================================================================
+    // FIELDS – VALIDATION CONSTRAINTS
+    // ==========================================================================================================
+
     @NotNull
     public Boolean filter;
 
+    // ==========================================================================================================
+    // CONSTRUCTORS
+    // ==========================================================================================================
+
     @JsonCreator
     public DataSource(
-            @JsonProperty(value = "filter", required = true) Boolean filter
+            @JsonProperty(value = FIELD_FILTER, required = true) Boolean filter
     ) {
         this.filter = filter;
     }
 
+    // Empty constructor - needed to be able to assign values manually
     public DataSource() {
     }
 }
