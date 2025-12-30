9c8af28cfb2d982bf3022be440d2b412ba98c5c1
bartlomiejfydrych <fydbar@gmail.com>
2025-12-30 20:20:33 +0100

    30.12.2025 (utils_tests): Zaktualizuj nazwę metody wywołującej endpoint {getLabel}

--- src/test/java/utils_tests/labels/PUT_UpdateLabelUtils.java
+++ src/test/java/utils_tests/labels/PUT_UpdateLabelUtils.java
@@ -5,7 +5,7 @@ import dto.labels.GET_GetLabelDto;
 import dto.labels.PUT_UpdateLabelDto;
 import io.restassured.response.Response;
 
-import static endpoints.labels.GET_GetLabelEndpoint.getGetLabel;
+import static endpoints.labels.GET_GetLabelEndpoint.getLabel;
 import static org.assertj.core.api.Assertions.assertThat;
 import static utils.UtilsCompare.compareObjects;
 import static utils.UtilsResponse.deserializeAndValidate;
@@ -29,7 +29,7 @@ public class PUT_UpdateLabelUtils extends TestBase {
     }
 
     public static void validateGetAgainstPut(PUT_UpdateLabelDto responsePutDto) {
-        Response responseGet = getGetLabel(responsePutDto.id);
+        Response responseGet = getLabel(responsePutDto.id);
         assertThat(responseGet.statusCode()).isEqualTo(200);
 
         GET_GetLabelDto responseGetDto = deserializeAndValidate(responseGet, GET_GetLabelDto.class);
