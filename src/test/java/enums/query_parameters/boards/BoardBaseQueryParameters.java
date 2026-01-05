6927131f6cbc8448a554dc2743a7c5d79ecda611
bartlomiejfydrych <fydbar@gmail.com>
2026-01-05 13:44:43 +0100

    05.01.2026 (enums): Dodaj enum {BoardBaseQueryParameters.java}

---
+++ src/test/java/enums/query_parameters/boards/BoardBaseQueryParameters.java
@@ -0,0 +1,42 @@
+package enums.query_parameters.boards;
+
+import enums.query_parameters.interfaces.boards.POST_CreateBoardQueryParam;
+import enums.query_parameters.interfaces.boards.PUT_UpdateBoardQueryParam;
+
+public enum BoardBaseQueryParameters implements POST_CreateBoardQueryParam, PUT_UpdateBoardQueryParam {
+
+    // ==========================================================================================================
+    // ENUMS
+    // ==========================================================================================================
+
+    // -----------------------
+    // COMMON QUERY PARAMETERS
+    // -----------------------
+
+    NAME("name"),
+    DESC("desc"),
+    ID_ORGANIZATION("idOrganization");
+
+    // ==========================================================================================================
+    // INTERNAL ENUM VALUE
+    // ==========================================================================================================
+
+    private final String key;
+
+    // ==========================================================================================================
+    // CONSTRUCTOR
+    // ==========================================================================================================
+
+    BoardBaseQueryParameters(String key) {
+        this.key = key;
+    }
+
+    // ==========================================================================================================
+    // QUERY PARAMETER ACCESSOR
+    // ==========================================================================================================
+
+    @Override
+    public String key() {
+        return key;
+    }
+}
